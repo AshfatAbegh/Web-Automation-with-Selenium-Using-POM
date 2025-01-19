b9f9c40b7c4f50c2de59dbce920f951a9f7c40d9
Ashfat Al Rashid <ashfat.rashid@polygontech.xyz>
2025-01-19 16:50:59 +0600

    Selenium Web Automation Using POM

--- src/main/java/com/demoqa/pages/alerts_frames_windows/Alerts_Frames_WindowsPage.java
+++ src/main/java/com/demoqa/pages/alerts_frames_windows/Alerts_Frames_WindowsPage.java
@@ -10,6 +10,13 @@ public class Alerts_Frames_WindowsPage extends HomePage {
 
     private By modalDialogsMenuItem = By.xpath("//div[@class='element-list collapse show']//li[@id='item-4']");
     private By alertsMenuItem = By.xpath("//li[@id='item-1']/span[text()='Alerts']");
+    private By framesMEnuItem = By.xpath("//li[@id='item-2']/span[text()='Frames']");
+
+    public FramesPage clickFrames(){
+        scrollToElementJS(framesMEnuItem);
+        click(framesMEnuItem);
+        return new FramesPage();
+    }
 
     public AlertsPage clickAlerts(){
         scrollToElementJS(alertsMenuItem);
---
+++ src/main/java/com/demoqa/pages/alerts_frames_windows/FramesPage.java
@@ -0,0 +1,20 @@
+package com.demoqa.pages.alerts_frames_windows;
+
+import org.openqa.selenium.By;
+
+public class FramesPage extends Alerts_Frames_WindowsPage {
+
+    private By textInFrame = By.id("sampleHeading");
+    private String iFrameBigBox = "frame1";
+
+    private void switchToBigBox(){
+        driver.switchTo().frame(iFrameBigBox);
+    }
+
+    public String getTextInBigFrame(){
+        switchToBigBox();
+        String bigFrameText = find(textInFrame).getText();
+        System.out.println(bigFrameText);
+        return bigFrameText;
+    }
+}
