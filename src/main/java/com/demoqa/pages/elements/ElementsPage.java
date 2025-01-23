393fb4efd96789c822b10305fd6397dc31a0605b
Ashfat Al Rashid <ashfat.rashid@polygontech.xyz>
2025-01-23 19:03:43 +0600

    Selenium Web Automation with POM

--- src/main/java/com/demoqa/pages/elements/ElementsPage.java
+++ src/main/java/com/demoqa/pages/elements/ElementsPage.java
@@ -3,10 +3,19 @@ package com.demoqa.pages.elements;
 import com.demoqa.pages.HomePage;
 import org.openqa.selenium.By;
 
+import static utilities.JavaScriptUtility.scrollToElementJS;
+
 public class ElementsPage extends HomePage {
 
     private By webTablesMenuItem = By.xpath("//div[@class='element-list collapse show']//li[@id='item-3']");
     private By linksMenuItem = By.xpath("//li[@id='item-5']/span[text()='Links']");
+    private By dynamicPropertiesMenuItem = By.xpath("//li[@id='item-8']/span[text()='Dynamic Properties']");
+
+    public DynamicPropertiesPage clickDynamicProperties(){
+        scrollToElementJS(dynamicPropertiesMenuItem);
+        click(dynamicPropertiesMenuItem);
+        return new DynamicPropertiesPage();
+    }
 
     public WebTablesPage clickWebTables(){
         click(webTablesMenuItem);
