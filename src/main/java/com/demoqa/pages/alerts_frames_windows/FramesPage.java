39eeb6a73f01ca72b79b4dc3bf7cbd85c6ab1011
Ashfat Al Rashid <ashfat.rashid@polygontech.xyz>
2025-01-23 13:40:53 +0600

    Selenium Web Automation with POM

--- src/main/java/com/demoqa/pages/alerts_frames_windows/FramesPage.java
+++ src/main/java/com/demoqa/pages/alerts_frames_windows/FramesPage.java
@@ -2,25 +2,42 @@ package com.demoqa.pages.alerts_frames_windows;
 
 import org.openqa.selenium.By;
 
+import static utilities.SwitchToUtility.*;
+
 public class FramesPage extends Alerts_Frames_WindowsPage {
 
     private By textInFrame = By.id("sampleHeading");
     private String iFrameBigBox = "frame1";
     private By headerFramesText = By.xpath("//div[@id='app']//h1[text()='Frames']");
+    private By iFrameSmallBox = By.xpath("//div[@id='frame2Wrapper']/iframe");
 
     public String getHeaderFramesText(){
         return find(headerFramesText).getText();
     }
 
     private void switchToBigBox(){
-        driver.switchTo().frame(iFrameBigBox);
+        //driver.switchTo().frame(iFrameBigBox); - line 18 & line 19 perform same action
+        switchToFrameString(iFrameBigBox);
+    }
+
+    private void switchToSmallBox(){
+        //switchToFrameIndex(2);
+        switchToFrameElement(find(iFrameSmallBox));
     }
 
     public String getTextInBigFrame(){
         switchToBigBox();
         String bigFrameText = find(textInFrame).getText();
         System.out.println(bigFrameText);
-        driver.switchTo().parentFrame();
+        switchToDefaultContent();
         return bigFrameText;
+    }
+
+    public String getTextInSmallFrame(){
+        switchToSmallBox();
+        String smallFrameText = find(textInFrame).getText();
+        System.out.println(smallFrameText);
+        switchToDefaultContent();
+        return smallFrameText;
     }
 }
